973c9ab2924f5daa60d181467fa5c2f92508698a
Aris Vangelis <avangelis@byte.gr>
2020-01-20 18:21:55 +0200

    adding header, logo and 2 options

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import HomePage from './pages/homePage/homePage.component'
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import ShopPage from './pages/shopPage/shopPage.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+
 
   );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './header.style.scss'
+
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+
+
+const Header = () => (
+    <div className="header">
+        <Link className='logo-container' to='/'>
+            <Logo className="logo" />
+        </Link>
+        <div className="options">
+            <Link to="/shop">
+                SHOP
+            </Link>
+            <Link className="option" to='/contact'> CONTACT
+            </Link>
+        </div>
+
+    </div>
+)
+
+export default Header
